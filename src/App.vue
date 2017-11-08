516d6d2a1d8cb8f6a8e91293940c4bd16605036d
azeupaul <azeupaul@gmail.com>
2017-11-08 09:39:12 +0100

    Setting up the routes

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="sideNav">
+    <v-navigation-drawer temporary v-model="sideNav">
       <v-list>
-        <v-list-tile v-for="item in menuItems" :key="item.title">
+        <v-list-tile
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.link"
+        >
           <v-list-tile-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-tile-action>
@@ -12,17 +16,24 @@
     </v-navigation-drawer>
     <v-toolbar dark class="primary">
       <v-toolbar-side-icon @click.stop="sideNav = !sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
-      <v-toolbar-title>DevMeetup</v-toolbar-title>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" style="cursor: pointer">DevMeetup</router-link>
+      </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-        <v-btn flat v-for="item in menuItems" :key="item.title">
+        <v-btn
+          flat
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.link"
+        >
           <v-icon dark left>{{ item.icon }}</v-icon>
           {{ item.title }}
         </v-btn>
       </v-toolbar-items>
     </v-toolbar>
     <main>
-      
+      <router-view></router-view>
     </main>
   </v-app>
 </template>
@@ -33,11 +44,11 @@
       return {
         sideNav: false,
         menuItems: [
-          {icon: 'supervisor_account', title: 'View Meetups'},
-          {icon: 'room', title: 'Organize Meetup'},
-          {icon: 'person', title: 'Organize Profile'},
-          {icon: 'face', title: 'Sign up'},
-          {icon: 'lock_open', title: 'Sign in'}
+          {icon: 'supervisor_account', title: 'View Meetups', link: '/meetups'},
+          {icon: 'room', title: 'Organize Meetup', link: '/meetup/create'},
+          {icon: 'person', title: 'Profile', link: '/profile'},
+          {icon: 'face', title: 'Sign up', link: '/signin'},
+          {icon: 'lock_open', title: 'Sign in', link: 'signup'}
         ]
       }
     }
---
+++ src/components/User/Profile.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p>The User profile page</p>
+  </div>
+</template>
