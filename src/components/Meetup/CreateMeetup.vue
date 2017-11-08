a157fa191f93c1f63bb87d52a02b6fc26b248ebb
azeupaul <azeupaul@gmail.com>
2017-11-08 14:55:00 +0100

    Start form create meetup

--- src/components/Meetup/CreateMeetup.vue
+++ src/components/Meetup/CreateMeetup.vue
@@ -1,5 +1,90 @@
 <template>
-  <div>
-    <p>The Create Meetup page</p>
-  </div>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <h3 class="accent--text">Create a new Meetup</h3>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-text-field
+          name="title"
+          label="Title"
+          id="title"
+          required
+          v-model="title"
+        ></v-text-field>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-text-field
+          name="location"
+          label="Location"
+          id="location"
+          required
+          v-model="location"
+        ></v-text-field>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-text-field
+          name="imageUrl"
+          label="Image url"
+          id="image-url"
+          required
+          v-model="imageUrl"
+        ></v-text-field>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <img :src="imageUrl" />
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-text-field
+          name="description"
+          label="Description"
+          id="description"
+          multiLine
+          required
+          v-model="description"
+        ></v-text-field>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-btn
+          class="primary"
+          :disabled="!formIsValid"
+        >
+          Create Meetup
+        </v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        title: '',
+        location: '',
+        imageUrl: '',
+        description: ''
+      }
+    },
+    computed: {
+      formIsValid () {
+        return this.title !== '' &&
+          this.location !== '' &&
+          this.imageUrl !== '' &&
+          this.description !== ''
+      }
+    }
+  }
+</script>
